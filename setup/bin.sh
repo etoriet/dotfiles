5ff3ebb8aea1c15abbe25cea5c453acc5bc39e22
eitaro.torii <eitaro.torii@wealthnavi.com>
2024-07-02 19:15:33 +0900

    cleanup

--- setup/bin.sh
+++ setup/bin.sh
@@ -14,7 +14,3 @@ mkdir -p "$HOME/bin"
 echo "installing scripts"
 cp "$SOURCE_DIR/../bin/tac" "$HOME/bin/"
 cp "$SOURCE_DIR/../bin/daily" "$HOME/bin/"
-echo "install gitlab-runner"
-curl --output "$HOME/bin/gitlab-runner" https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-darwin-amd64
-chmod +x "$HOME/bin/gitlab-runner"
-# TODO gcloud
